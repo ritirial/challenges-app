84efed72779d9ba608747e09c239699fec736951
Ricardo Ricardez <ritirial@gmail.com>
2023-11-27 12:44:42 -0600

    corrección de routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
-import { RouterTestingModule } from "@angular/router/testing";
 
 @Component({
   selector: 'app-root',
@@ -11,7 +10,6 @@ import { RouterTestingModule } from "@angular/router/testing";
     RouterOutlet,
     RouterLink,
     RouterLinkActive,
-    RouterTestingModule
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
