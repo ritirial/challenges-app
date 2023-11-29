9a1b83f995fddd2897b58b36a1b6d359ac743720
Ricardo Ricardez <ritirial@gmail.com>
2023-11-29 04:56:42 -0600

    implementar cypress para e2e testing, ajuste de componentes

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,11 +23,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('challenges-app');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Challenges Apps Integration Testing');
-  });
 });
